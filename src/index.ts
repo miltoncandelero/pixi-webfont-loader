b030a4f56e56a184eb9e79f85dbc1a826b011984
Milton <miltoncandelero@gmail.com>
2021-08-13 12:39:42 -0300

    changing the PIXI namespace

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import '../global.d.ts';
-import { Loader } from '@pixi/loaders';
 
 import  WebfontLoaderPlugin  from './WebfontLoaderPlugin';
-
-Loader.registerPlugin(WebfontLoaderPlugin);
 
 export { WebfontLoaderPlugin };
